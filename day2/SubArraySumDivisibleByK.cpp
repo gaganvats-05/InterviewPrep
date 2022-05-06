b35f8abe2e6455b2776430caf1915b61b55519e3
Gagan Vats <gaganvats05@gmail.com>
2022-05-06 07:27:44 +0530

    SubArraySumDivisibleByK

---
+++ day2/SubArraySumDivisibleByK.cpp
@@ -0,0 +1,19 @@
+// Question Link - https://leetcode.com/problems/subarray-sums-divisible-by-k/
+#include <bits/stdc++.h>
+using namespace std;
+
+// use hash map and prefix sum with modular arithematic
+int subarraysDivByK(vector<int>& nums, int k) {
+    int n=nums.size();
+    int cur_sum=0;
+    int ans=0;
+    unordered_map<int,int>umap;
+    for (int i = 0; i < n; i++)
+    {
+        cur_sum=((cur_sum+nums[i])%k+k)%k;
+        if(cur_sum==0)ans++;
+        ans+=umap[cur_sum];
+        umap[cur_sum]++;
+    }
+    return ans;
+}
